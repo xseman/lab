ce224605589aba1d19dc1ae1f8f20e48d09dfe0a
Filip Seman <filip.seman@protonmail.com>
2022-06-09 11:03:54 +0200

    go: update named return

--- go/return/named/main.go
+++ go/return/named/main.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
-
-func main() {
-	foo := Foo()
-	fmt.Println(foo)
-}
+import (
+	"fmt"
+)
 
 // Go's return values may be named.
 // If so, they are treated as variables defined at the top of the function.
 func Foo() (foo int) {
 	foo++
 	return foo
+
+	// We can also just call return, which is same behaviour
+	// return
+}
+
+func main() {
+	foo := Foo()
+	fmt.Println(foo)
 }
