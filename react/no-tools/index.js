e7cc055462998fc8618d573d697363c12ee9d1a2
Filip Seman <filip.seman@protonmail.com>
2023-04-27 08:44:10 +0200

    react: update no-tool src esm.sh

--- react/no-tools/index.js
+++ react/no-tools/index.js
@@ -1,16 +1,18 @@
+/**
+ * Resources:
+ * - https://reactjs.org/docs/add-react-to-a-website.html
+ * - https://gist.github.com/gaearon/faa67b76a6c47adbab04f739cba7ceda
+ */
+
+import React from "https://esm.sh/react@18.2.0"
+import ReactDOM from "https://esm.sh/react-dom@18.2.0"
+
 import Counter from './Counter.js';
 
 const h = React.createElement;
 
-export function main() {
+export function init() {
     const rootElement = document.getElementById("root");
-
     const root = ReactDOM.createRoot(rootElement);
     root.render(h(Counter, null));
 }
-
-/**
- * Related:
- * - https://reactjs.org/docs/add-react-to-a-website.html
- * - https://gist.github.com/gaearon/faa67b76a6c47adbab04f739cba7ceda
- */
